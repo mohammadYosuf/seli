bc95a5d398db3a916a6e4ef61897a3f65d224812
Esmailzadeh <m.yosuf2000@gmail.com>
2023-06-28 00:46:01 +0330

    add timer until page open

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
 #in this state create driver for google chrome and open login page
 driver = webdriver.Chrome()
 driver.get("https://panel.iranicard.ir/login")
+time.sleep(70)
+
