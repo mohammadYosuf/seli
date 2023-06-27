2dcf213e4b766c6f3cbfd45202907f4ee15b38fd
Esmailzadeh <m.yosuf2000@gmail.com>
2023-06-27 20:25:50 +0330

    init new change create driver for google chrome and open login page

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #install selenium with pip install selenium
+#import selenium webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
+#in this state create driver for google chrome and open login page
+driver = webdriver.Chrome()
+driver.get("https://panel.iranicard.ir/login")
